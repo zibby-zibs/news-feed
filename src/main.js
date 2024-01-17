a5b11820a4fe00a1237bfd8ce3f79734d12f9f52
zibby-zibs <oloyedetimi@gmail.com>
2024-01-17 14:35:27 +0100

    fixed store error

--- src/main.js
+++ src/main.js
@@ -2,12 +2,10 @@ import { createApp } from "vue";
 import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
-import store from "./store";
 import "./css/main.css";
 
 const app = createApp(App);
 const pinia = createPinia();
-app.use(store);
 app.use(router);
 app.use(pinia);
 
